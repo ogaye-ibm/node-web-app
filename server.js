781dbdb45a09f8b8fc6eab2980c9b9d906eee284
Omar Gaye <omar.gaye@ibm.com>
2020-05-23 19:20:56 -0400

    Clean up

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello CP4A Chapter !!!');
+    res.send('Hello CP4A Chapter!');
 });
 
 app.listen(PORT, HOST);
